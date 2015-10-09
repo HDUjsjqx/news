bdd4a441b9a095f5ab74a6d3b9de95f9baf86f2c
lord63 <lord63.j@gmail.com>
2015-10-09 10:57:36 +0800

    Change the siteurl
    we won't renew hdujsjqx.org anymore  :(

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,7 @@ sys.setdefaultencoding('utf-8')
 
 AUTHOR = u'HDU 计算机青协'
 SITENAME = u"News"
-SITEURL = 'http://news.hdujsjqx.org'
+SITEURL = 'https://hdujsjqx.github.io/news'
 
 PATH = 'content'
 
@@ -19,21 +19,10 @@ DEFAULT_LANG = u'ZH'
 
 THEME = 'Just-Read'
 
-
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
